8803a3eed9865f539cb198c3bc460e2fc432dc60
Krishnath <shadyanuj24@gmail.com>
2020-01-02 22:12:23 +0530

    first commit

---
+++ v1/app.js
@@ -0,0 +1,36 @@
+var express=require('express');
+const app=express();
+
+
+
+app.set("view engine","ejs");
+
+app.get('/',function(req,res){
+
+    res.render('landing');
+
+});
+
+app.get('/campgrounds',(req,res)=>{
+    var campgrounds=[
+        {name: "Salmon Creek",image: "https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+        {name: "Granite Hill",image: "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"},
+        {name: "Mountain Goat's Rest",image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"}
+    ];
+
+    res.render("campgrounds",{campgrounds: campgrounds});
+
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("YelpCamp Server started!");
+});
