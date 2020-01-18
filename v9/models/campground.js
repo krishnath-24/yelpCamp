eb24a36746586a7663ac91a142e37ce493281c2e
Krishnath <shadyanuj24@gmail.com>
2020-01-18 10:48:05 +0530

    started with the version 9

---
+++ v9/models/campground.js
@@ -0,0 +1,26 @@
+var mongoose=require('mongoose');
+
+//CampGround Schema Setup
+var campgroundSchema=mongoose.Schema({
+    name:String,
+    image:String,
+    description:String,
+    author : {
+
+        id : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User"
+        },
+        username : String
+    },
+    comments:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Comment"
+        }
+    ]
+});
+
+//export the campground model
+
+module.exports= mongoose.model("campground",campgroundSchema);
