af54ff95cf3382aa51a400b1914d05d75aea5ac0
Krishnath <shadyanuj24@gmail.com>
2020-01-19 11:06:48 +0530

    added the middleware in v9 and refactored the middlewares in v10 to a directory

--- v10/middleware/index.js
+++ v10/middleware/index.js
@@ -2,7 +2,65 @@
 
 var middlewares = {};
 
-middlewares.checkCamgroundOwnership = function(){
+middlewares.checkCamgroundOwnership = function(req,res,next){
 
-    
+    if(req.isAuthenticated()){
+
+        Campground.findById(req.params.id,function(err,foundCampground){
+
+            if(err){
+                console.log(err);
+                res.redirect("back");   
+            } else{
+
+                if(foundCampground.author.id.equals(req.user._id)){
+
+                    next();
+
+                } else{
+                    res.redirect("back");
+                }
+            }
+        });
+
+    } else{
+        res.redirect("back");
+    }
+}
+
+middlewares.checkCommentOwnership = function(req,res,next){
+    if(req.isAuthenticated()){
+
+        Comment.findById(req.params.Comment_id,function(err,foundComment){
+
+            if(err){
+                console.log(err);
+                res.redirect("back");   
+            } else{
+
+                if(foundComment.author.id.equals(req.user._id)){
+
+                    next();
+
+                } else{
+                    res.redirect("back");
+                }
+            }
+        });
+
+    } else{
+        res.redirect("back");
+    }
+}
+
+middlewares.isLoggedIn = function(req,res,next){
+
+    if(req.isAuthenticated()){
+
+        next();
+
+    } else{
+
+        res.redirect("back");
+    }
 }
--- v9/routes/comments.js
+++ v9/routes/comments.js
@@ -101,4 +101,35 @@ function isLoggedIn(req,res,next){
         res.redirect('/login'); 
     }
 }
+
+
+function checkCommentOwnership(req,res,next){
+    if(req.isAuthenticated()){
+
+        Comment.findById(req.params.comment_id,function(err, foundComment){
+
+            if(err){
+                console.log(err);
+                res.redirect("back");
+            } else{
+                
+                if(foundComment.author.id.equals(req.user._id)){
+                    
+                    next();
+                    
+                } else{
+                    res.redirect("back");
+                }
+            }
+            
+        });
+        
+    } else{
+
+        res.redirect("back");
+    }
+
+}
+
+
 module.exports = router;
